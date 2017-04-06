bb8ff61a4b3fcdbe18dd33589bb6c40263c65279
George Antonious <george@antonious.ca>
2017-04-06 16:39:05 -0600

    Got dotnet ef working
    - Turns out dotnet core changes every other day so you dont actually know
      how to get it to work

--- DataAccess/BlogDatabaseContext.cs
+++ DataAccess/BlogDatabaseContext.cs
@@ -5,8 +5,13 @@ namespace Blogs.DataAccess
 {
     public class BlogDatabaseContext : DbContext
     {
+        public BlogDatabaseContext()
+        {
+
+        }
+
         public BlogDatabaseContext(DbContextOptions<BlogDatabaseContext> options) : base(options)
-        {  
+        {
         }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<User> Users { get; set; }
