5282a60e2bb7f4cf3db8c1e6ab6647418d0b7d95
Jessica Prieto <jessicaprieto31@gmail.com>
2017-04-06 11:59:52 -0600

    resourcing the right location for DbContext in ValuesController

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blogs.Models;
+using Blogs.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blogs.Controllers
