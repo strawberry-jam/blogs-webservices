e5f77ea02e79c23707fde1ec6d33eb0386d0c73b
Jessica Prieto <jessicaprieto31@gmail.com>
2017-04-06 11:42:49 -0600

    moved DBContext to a different folder

---
+++ DataAccess/BlogDatabaseContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Blogs.Models;
+
+namespace Blogs.DataAccess
+{
+    public class BlogDatabaseContext : DbContext
+    {
+        public BlogDatabaseContext(DbContextOptions<BlogDatabaseContext> options) : base(options)
+        {  
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
