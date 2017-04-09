abd1aacd9b84b4c0191e275bee82c5893d7ecfa4
jessicaprt <jessicaprieto31@gmail.com>
2017-04-09 09:49:03 -0600

    added BlogController

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+using System;
+using Blogs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Blogs.Services;
+
+namespace Blogs.Controllers
+{
+    [RouteAttribute("api/v1/blogs/{blogId}")]
+    public class BlogController : Blog
+    {
+        private readonly IBlogService _blogService;
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet("{blogId")]
+        public Blog Get(string blogId)
+        {
+            return _blogService.Get(blogId);
+        }
+
+        [HttpPost]
+        public void Create(Blog blog)
+        {
+            blog.Id = Guid.NewGuid().ToString();
+            _blogService.Create(blog);
+
+        }
+
+        [HttpPut]
+        public void Update(Blog blog)
+        {
+            _blogService.Update(blog);
+        }
+
+        [HttpDeleteAttribute("{blogId")]
+        public void Delete(string blogId)
+        {
+            _blogService.Delete(blogId);
+        }
+    }
+}
